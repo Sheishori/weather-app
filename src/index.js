8d8fcf9f5d848999666f0f1c5b0421dc66e84cee
Sheishori <sheishori@gmail.com>
2022-12-02 15:38:16 +0100

    Add function returning only required weather data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
-async function getWeather(location = 'London') {
+async function getWeatherAPIData(location = 'London') {
 	const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=London&APPID=31cc72b5a0cd8e85f3b76bb2c36522ed', {mode: 'cors'});
-	const weatherData = await response.json();
-	console.log(weatherData);
+	const APIData = await response.json();
+	return APIData;
 };
 
-getWeather();
+async function processWeatherData() {
+	const APIData = await getWeatherAPIData();
+	const weatherData = {
+		name: APIData.name,
+		country: APIData.sys.country,
+		weather: APIData.weather[0].description,
+		temp: APIData.main.temp,
+		pressure: APIData.main.pressure,
+		humidity: APIData.main.humidity,
+		wind: APIData.wind.speed,
+	};
+	return weatherData;
+};
+
+console.log(processWeatherData());
