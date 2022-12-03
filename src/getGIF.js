70080ffc78d340f4ff82d9fd4500e91788a8ce2f
Sheishori <sheishori@gmail.com>
2022-12-03 12:06:00 +0100

    Add more error handling to getGIF module

--- src/getGIF.js
+++ src/getGIF.js
@@ -2,7 +2,11 @@ async function getGIF(weather) {
 	try {
 		const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=ekXLfmy8SXYal69CoD65HEjmmOZDu7Fh&s=${weather}`, {mode: 'cors'});
 		const imgData = await response.json();
-		return imgData.data.images.original.url;
+		if (imgData.meta.status === 200) {
+			return imgData.data.images.original.url;
+		} else {
+			console.log(imgData.meta.msg);
+		}
 	} catch (error) {
 		console.log(error);
 	};
